94e11172112916e3ff20d54f1396c3f5883aeca5
Grady Carlson <26gradycarlson@usd266.com>
2023-12-01 10:13:07 -0600

    did even more problems

--- 06ListsAndLoops/Assignment/main.py
+++ 06ListsAndLoops/Assignment/main.py
@@ -41,3 +41,73 @@ def average_act_score(scores):
 print("Average ACT Scores:")
 print("[12, 36, 32, 24, 15] =>", average_act_score([12, 36, 32, 24, 15]))
 print("")
+
+def all_true(words):
+    falses = 0
+    for word in words:
+        if word == False:
+            falses = falses + 1
+        else: 
+            falses = falses + 0
+    if falses > 0:
+         return False
+    else:
+         return True
+           
+print("All True:")
+print("[True,False,False] =>", all_true([True,False,False]))
+print("[True,True,True] =>", all_true([True,True,True]))
+print("[False,True,True] =>", all_true([False,True,True]))
+
+print("")
+
+def any_true(words):
+    trues = 0
+    for word in words:
+        if word == True:
+            trues = trues + 1
+    if trues > 0:
+        return True
+    else:
+        return False
+    
+print("Any True:")
+print("[False, False, False, True] =>", any_true([False, False, False, True]))
+print("[False, False, False, False] =>", any_true([False, False, False, False]))
+print("")
+
+def mostly_true(words):
+    trues = 0
+    falses = 0
+    for word in words:
+        if word == True:
+            trues = trues + 1
+        else:
+            falses = falses + 1
+    if trues > falses:
+        return True
+    else:
+        return False
+    
+print("Mostly True:")
+print("[True,False,False] =>", mostly_true([True,False,False]))
+print("[True,True,True] =>", mostly_true([True,True,True]))
+print("[False,True,True] =>", mostly_true([False,True,True]))
+print("")
+
+def has_vowel(letters):
+    vowels = 0
+    for letter in letters:
+        if letter in ["a","e","i","o","u"]:
+            vowels = vowels + 1
+        else:
+            vowels = vowels + 0
+    if vowels > 0:
+        return True
+    else:
+        return False
+
+print("Has Vowels:")
+print("[g,r,a,d,y] =>", has_vowel(["g","r","a","d","y"]))    
+print("[g,r,n,d,y] =>", has_vowel(["g","r","n","d","y"])) 
+print("")
