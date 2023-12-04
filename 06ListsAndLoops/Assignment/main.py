54b7488e03247b65df14ceeff9309ea84cbbbe1f
Grady Carlson <26gradycarlson@usd266.com>
2023-12-04 10:15:03 -0600

    did smore smore problems :thumbs upo:

--- 06ListsAndLoops/Assignment/main.py
+++ 06ListsAndLoops/Assignment/main.py
@@ -114,3 +114,82 @@ print("Has Vowels:")
 print("[g,r,a,d,y] =>", has_vowel(["g","r","a","d","y"]))    
 print("[g,r,n,d,y] =>", has_vowel(["g","r","n","d","y"])) 
 print("")
+
+def all_the_same(words):
+    same = True
+    first = words[0]
+    for word in words:
+        if word == first:
+            pass
+        else:
+            same = False
+    return same
+
+print("All the Same:")
+print("[7,7,7] =>", all_the_same([7,7,7]))
+print("[5,5,5,5,3,4,65,76] =>", all_the_same([5,5,5,5,3,4,65,76]))
+print("")
+
+def increasing(numbers):
+    increasing = True
+    smaller = -9999999999
+    for number in numbers:
+        if int(number) > int(smaller):
+            smaller = number
+            pass
+        else:
+            increasing = False
+    return increasing
+
+print("Increasing:")
+print("[1,2,3,4,5,6]", increasing([1,2,3,4,5,6]))
+print("[4,3,65,87,65,4]", increasing([4,3,65,87,65,4]))
+print("")
+
+def is_incrementing(numbers):
+    prev = numbers[0]-1
+    incrementing = True
+    for number in numbers:
+        if int(number) == int(prev) + 1:
+            prev = number
+            pass
+        else:
+            incrementing = False
+    return incrementing
+
+print("Incrementing:")
+print("[1,2,3,4,5,6]", is_incrementing([1,2,3,4,5,6]))
+print("[4,3,65,87,65,4]", is_incrementing([4,3,65,87,65,4]))
+print("")
+
+def has_adjacent_repeat(numbers):
+    adjacent = False
+    prev = 0
+    for number in numbers:
+        if int(number) == int(prev):
+            adjacent = True
+        else:
+            prev = number
+            pass
+    return adjacent
+
+print("Is Adjacent:")
+print("[1,2,3,4,5,5,6]", has_adjacent_repeat([1,2,3,4,5,5,6]))
+print("[1,2,3,4,5,6]", has_adjacent_repeat([1,2,3,4,5,6]))
+print("")
+
+def sum_with_skips(numbers):
+    ignoring = False
+    sum = 0
+    for number in numbers:
+        if ignoring == False and number == -1:
+            ignoring = True
+        elif ignoring == True and number == -1:
+            ignoring = False
+        elif ignoring == False:
+            sum = sum + number
+    return sum
+
+print("Sum with Skips:")
+print("[4,-1,4,-1,4,1] =>", sum_with_skips([4,-1,4,-1,4, 1]))
+print("")
