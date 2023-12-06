da3e97dbcf3d6a359054c36853c61f9dcb4ce86b
Grady Carlson <26gradycarlson@usd266.com>
2023-12-06 09:53:40 -0600

    did list problems

--- 07StringsAsLists/Assignment/main.py
+++ 07StringsAsLists/Assignment/main.py
@@ -62,3 +62,90 @@ print("In Alphabetical Order:")
 print("abcd =>", in_alphabetical_order("abcd"))
 print("dtbgtjfhy =>", in_alphabetical_order("dtbgtjfhy"))
 print("")
+
+def alternate_case(word):
+    upper = True
+    result = ""
+    for letter in word:
+        if upper == False:
+            result = result + letter.lower()
+            upper = True
+        else:
+            result = result + letter.upper()
+            upper = False
+    return result
+
+print("Alternating Case:")
+print("grady carlson =>", alternate_case("grady carlson"))
+print("")
+
+def remove_vowels(word):
+    result = ""
+    for letter in word:
+        if letter in "aeiou":
+            pass
+        else:
+            result = result + letter
+    return result
+
+print("Remove Vowels:")
+print("Grady Carlson =>", remove_vowels("Grady Carlson"))
+print("")
+
+def to_camel_case(phrase):
+    upp = False
+    result = ""
+    for word in phrase:
+        if word == " ":
+            upp = True
+        elif upp == True:
+            result = result + word.upper()
+            upp = False
+        else:
+            result = result + word
+    return result
+
+print("To Camel Case:")
+print("Grady Carlson is cool =>", to_camel_case("Grady Carlson is cool"))
+print("")
+
+def to_snake_case(phrase):
+    result = ""
+    for word in phrase:
+        if word == " ":
+            result = result + "_"
+        else:
+            result = result + word
+    return result
+
+print("To Snake Case:")
+print("Grady Carlson is cool =>", to_snake_case("Grady Carlson is cool"))
+print("")
+
+def without_duplications(numbers):
+    same = ""
+    result = []
+    for number in numbers:
+        if number == same:
+            pass
+        else:
+            result.append(number)
+            same = number
+    return result
+
+print("Without Duplications:")
+print("122345677778899 =>", without_duplications([1,2,2,3,4,5,6,7,7,7,7,8,8,9,9]))
+print("")
+
+def filter_valid_act_scores(scores):
+    result = []
+    for score in scores:
+        if 0<score<37:
+            result.append(score)
+        else:
+            pass
+    return result
+
+print("Filter Valid ACT Scores:")
+print("13,0,39,36,42,24,32,54", filter_valid_act_scores([13,0,39,36,42,24,32,54]))
+print("")
